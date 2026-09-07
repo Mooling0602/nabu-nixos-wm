ed4abe08a4f0a8618d080e747f5d1c5feca99006
Mooling0602 <clemooling@outlook.com>
2026-09-08 00:00:44 +0800

    feat: add nice terminal apps

--- nixos/home.nix
+++ nixos/home.nix
@@ -15,12 +15,15 @@
   };
   home.stateVersion = "26.11";
   home.packages = with pkgs; [
+    kitty
+    alacritty
     zellij
     fastfetch
     qq
     wechat
     telegram-desktop
     # zed # not friendly for touchscreen users
+    google-chrome
     vscode
     gh
     kdePackages.ksshaskpass
